2647c17e72b0239fc95807333d33b7fcc007186a
RusJJ <mctesting1@yandex.ru>
2025-08-22 14:33:24 +0300

    Some structs and stuff

--- soundscape.cpp
+++ soundscape.cpp
@@ -1,6 +1,54 @@
 #include "soundscape.h"
 
-void CSoundScape::LoadDat()
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <rapidjson/include/rapidjson/document.h>
+#include <rapidjson/include/rapidjson/filereadstream.h>
+
+CSoundScape g_SoundScapes[MAX_SOUNDSCAPES] { 0 };
+SoundDef g_SoundDefinitions[MAX_SOUNDSCAPES * MAX_SOUNDSCAPES_SOUNDS] { 0 };
+
+bool CSoundScape::LoadDat(const char* filepath)
 {
-    
+    FILE* jsonFile = fopen(filepath, "rb");
+    if(!jsonFile) return false;
+
+    fseek(jsonFile, 0, SEEK_END);
+    long len = ftell(jsonFile);
+    fseek(jsonFile, 0, SEEK_SET);
+
+    char* buf = new char[len + 1];
+    if(!buf)
+    {
+        fclose(jsonFile);
+        return false;
+    }
+
+    rapidjson::FileReadStream is(jsonFile, buf, len+1);
+    rapidjson::Document doc;
+    doc.ParseStream(is);
+    delete[] buf;
+
+    if(doc.HasParseError() || !doc.IsObject())
+    {
+        fclose(jsonFile);
+        return false;
+    }
+
+    for (rapidjson::Value::ConstMemberIterator itr = doc.MemberBegin(); itr != doc.MemberEnd(); ++itr)
+    {
+        const rapidjson::Value& innerObj = itr->value;
+        if(innerObj.IsObject()) // SoundScape definition (most likely.)
+        {
+
+        }
+        else // Config or some sort of stuff
+        {
+
+        }
+    }
+
+    fclose(jsonFile);
+    return true;
 }
--- soundscape.h
+++ soundscape.h
@@ -1,9 +1,63 @@
 #ifndef __SOUNDSCAPE_H
 #define __SOUNDSCAPE_H
 
+#define MAX_SOUNDSCAPES                 1024
+#define MAX_SOUNDSCAPES_SOUNDS          6
+#define MAX_SOUNDSCAPES_ACTIVE_SOUNDS   32
+#define MAX_SOUNDSCAPES_SOUND_PATH      160
+
+enum eSoundScapeType : unsigned char
+{
+    SST_UNKNOWN = 0,
+    SST_BOX,
+    SST_SPHERE,
+    SST_DOME,
+
+    SST_MAXTYPES
+};
+
+struct Pos3D
+{
+    float x, y, z;
+};
+
+struct SoundScapeBox
+{
+    Pos3D m_vecStart, m_vecEnd;
+};
+struct SoundScapeSphere
+{
+    Pos3D m_vecCenter;
+    float m_fRadius;
+};
+struct SoundDef
+{
+    char m_szSoundPath[MAX_SOUNDSCAPES_SOUND_PATH];
+    void* m_pSoundPtr;
+    Pos3D m_vecCenter;
+    float m_fVolume;
+    unsigned int m_nFadeOutTime; // Appearing time
+    unsigned int m_nFadeInTime; // Disappearing time
+    bool m_bLooped;
+    bool m_bActive;
+};
+
 struct CSoundScape
 {
-    static void LoadDat();
+    static bool LoadDat(const char* filepath);
+    static inline int m_nSoundScapes = 0;
+
+    int m_nID;
+    eSoundScapeType m_nType = SST_UNKNOWN;
+    bool m_bActive;
+    unsigned char m_nSounds;
+
+    union m_Soundscape
+    {
+        SoundScapeBox m_box;
+        SoundScapeSphere m_sphere;
+    };
+    SoundDef* m_SoundDef[MAX_SOUNDSCAPES_SOUNDS];
 };
 
 #endif // __SOUNDSCAPE_H
