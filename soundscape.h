f999040d4769e0d8ff8d03f03351d202e69c436e
[-=KILL MAN=-] <mctesting1@yandex.ru>
2025-08-23 08:49:27 +0300

    Update soundscape.h

--- soundscape.h
+++ soundscape.h
@@ -4,8 +4,9 @@
 #include <mod/logger.h>
 
 #define MAX_SOUNDSCAPES                 1024
+#define MAX_ACTIVE_SOUNDSCAPES          10
 #define MAX_SOUNDSCAPES_SOUNDS          6
-#define MAX_SOUNDSCAPES_ACTIVE_SOUNDS   32
+#define MAX_SOUNDSCAPES_ACTIVE_SOUNDS   (2 * MAX_ACTIVE_SOUNDSCAPES * MAX_SOUNDSCAPES_SOUNDS)
 #define MAX_SOUNDSCAPES_SOUND_PATH      160
 
 struct CSoundScape;
@@ -43,6 +44,7 @@ struct SoundDef
     void* m_pSoundPtr;
     Pos3D m_vecCenter;
     float m_fVolume; // [0.0 - 1.0]
+	float m_fDistance;
     unsigned int m_nFadeOutTime; // Appearing time (ms)
     unsigned int m_nFadeInTime; // Disappearing time (ms)
     bool m_bLoaded;
@@ -53,18 +55,28 @@ struct SoundDef
 
 struct CSoundScape
 {
+	// Static
     static bool StaticInit();
     static bool LoadDat(const char* filepath);
     static void UpdateCamera(Pos3D pos, Pos3D front, Pos3D top);
+	static CSoundScape* New();
 
     static inline int m_nSoundsUsed = 0;
     static inline int m_nSoundScapes = 0;
+	static inline int m_nActiveSoundScapes = 0;
     static inline Pos3D m_vecCameraPos;
     static inline Pos3D m_vecCameraFront;
     static inline Pos3D m_vecCameraTop;
 
 
 
+	// Member
+	SoundDef* AttachSoundDef();
+	int GetActiveListPos();
+	bool InActiveList();
+	bool Activate();
+	bool Deactivate();
+	
     int m_nID;
     eSoundScapeType m_nType = SST_UNKNOWN;
     bool m_bActive;
