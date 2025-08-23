086928dec9f54840c054fc02bbd72cdf0eed934b
[-=KILL MAN=-] <mctesting1@yandex.ru>
2025-08-23 08:49:54 +0300

    Update soundscape.cpp

--- soundscape.cpp
+++ soundscape.cpp
@@ -7,7 +7,10 @@
 #include <rapidjson/include/rapidjson/filereadstream.h>
 
 CSoundScape g_SoundScapes[MAX_SOUNDSCAPES] { 0 };
+CSoundScape* g_ActiveSoundScapes[MAX_ACTIVE_SOUNDSCAPES] { NULL };
 SoundDef g_SoundDefinitions[MAX_SOUNDSCAPES * MAX_SOUNDSCAPES_SOUNDS] { 0 };
+
+// Static
 
 bool CSoundScape::StaticInit()
 {
@@ -68,4 +71,70 @@ void CSoundScape::UpdateCamera(Pos3D pos, Pos3D front, Pos3D top)
     m_vecCameraPos = pos;
     m_vecCameraFront = front;
     m_vecCameraTop = top;
+}
+
+CSoundScape* CSoundScape::New()
+{
+	return &g_SoundScapes[m_nSoundScapes++];
+}
+
+// Member
+
+SoundDef* CSoundScape::AttachSoundDef()
+{
+	if(m_nSounds >= MAX_SOUNDSCAPES_SOUNDS) return NULL;
+	
+	SoundDef* attachment = &g_SoundDefinitions[m_nSoundsUsed++];
+	m_SoundDef[m_nSounds++] = attachment;
+	return attachment;
+}
+
+int CSoundScape::GetActiveListPos()
+{
+	for(int i = 0; i < m_nActiveSoundScapes; ++i)
+	{
+		if(g_ActiveSoundScapes[i] == this) return i;
+	}
+	return -1;
+}
+
+bool CSoundScape::InActiveList()
+{
+	return (GetActiveListPos() != -1);
+}
+
+bool CSoundScape::Activate()
+{
+	if(m_nActiveSoundScapes >= MAX_ACTIVE_SOUNDSCAPES || InActiveList()) return false;
+	
+	m_bActive = true;
+	g_ActiveSoundScapes[m_nActiveSoundScapes++] = this;
+	for(int i = 0; i < m_nSounds; ++i)
+	{
+		// TODO: Activate sound sources?
+	}
+	return true;
+}
+
+bool CSoundScape::Deactivate()
+{
+	m_bActive = false;
+	int listPos = GetActiveListPos();
+	if(listPos == -1) return false;
+	
+	if(m_nActiveSoundScapes > 1)
+	{
+		g_ActiveSoundScapes[listPos] = g_ActiveSoundScapes[--m_nActiveSoundScapes];
+	}
+	else
+	{
+		g_ActiveSoundScapes[0] = NULL;
+		m_nActiveSoundScapes = 0;
+	}
+	
+	for(int i = 0; i < m_nSounds; ++i)
+	{
+		// TODO: Deactivate sound sources?
+	}
+	return true;
 }
